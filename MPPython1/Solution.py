145c3a3c0bfe8e9a4a7212aa758f5a3d79d14ee0
breckenmcgeough <89755831+breckenmcgeough@users.noreply.github.com>
2022-04-04 20:55:24 -0400

    problem 1

--- MPPython1/Solution.py
+++ MPPython1/Solution.py
@@ -17,7 +17,9 @@ class Solution:
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
         """
-        paths, bandwidths, priorities = {}, {}, {}
+        output = bfs_path(self.graph,self.isp,self.info["list_clients"])
+        print(output)
+        paths, bandwidths, priorities = output, {}, {}
         # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
         # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
         return (paths, bandwidths, priorities)
